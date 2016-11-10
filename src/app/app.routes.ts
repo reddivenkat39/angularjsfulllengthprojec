2b9cd26874e6dacc24a98bdd30e6b9497bc7c00f
chandu <chandu.adabala@tabnergc.com>
2016-11-10 16:11:51 -0500

    my changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
-// /**
-//  * Created by bapirazz naidu on 11/8/2016.
-//  */
-// import { RouterModule,Routes} from "@angular/router";
-// import { HeaderComponent } from './header/header.component';
-//
-// const APP_ROUTES: Routes = [
-//   { path: 'header', component: HeaderComponent }
-// ];
-//
-//
-// export const routing =RouterModule.forRoot(APP_ROUTES);
+/**
+ * Created by bapirazz naidu on 11/8/2016.
+ */
+import { RouterModule,Routes} from "@angular/router";
+
+import {LoginComponent} from "./staff/login/login.component";
+import {HeaderComponent} from "./header/header.component";
+
+const APP_ROUTES: Routes = [
+  { path: '', component: LoginComponent },
+  {path:'', component: HeaderComponent}
+];
+
+
+export const routing =RouterModule.forRoot(APP_ROUTES);
