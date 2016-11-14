aadfde427b5b0b4744ec6563b3b75599723ae33b
chandu.adabala <chandu.adabala@tabnergc.com>
2016-11-14 17:17:16 -0500

    Merge branch 'feature/sprintNov' of http://10.10.5.33:7990/scm/por/tabnerportalui into feature/sprintNov
    # Conflicts:
    #	src/app/staff/login/login.component.html
    #	src/app/staff/login/login.component.spec.ts
    #	src/app/staff/login/login.component.ts
    #	src/app/staff/staffservices/staff.service.ts

---
+++ src/app/staffHome/candidates/candidates.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers, Response} from "@angular/http";
+
+@Injectable()
+export class CandidatesService {
+
+  constructor(private http: Http) {
+  }
+
+  sendScreeningDetails(myForm) {
+    console.log("send Screening details : screening ", myForm);
+    const bodySend = JSON.stringify(myForm);
+    console.log("myForm body ...........", bodySend)
+    const headersSend = new Headers({'Content-Type': 'application/json'});
+    return this.http.post("http://localhost:8080/savescreening", bodySend, {headers: headersSend}).map((res: Response)=> {
+        console.log("send screening deails", +res);
+        console.log("only key value pairs", res.json().value.candidateData);
+        return res.json()
+
+      }
+    );
+  }
+
+}
