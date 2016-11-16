a5d7bb11d7244058d487c540778b4e1fef94c210
bindu bhupathi <bindubhupathi12@gmail.com>
2016-11-16 12:41:04 -0500

    sign page ui and backend integration.

---
+++ src/app/staff/addstaff/addstaff.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
+import {AddstaffService} from "../staffservices/addstaff.service";
+@Component({
+  selector: 'app-addstaff',
+  templateUrl: './addstaff.component.html',
+  styleUrls: ['./addstaff.component.css']
+})
+export class AddstaffComponent {
+  myAddStaffForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private addStaffService:AddstaffService) {
+    this.myAddStaffForm = formBuilder.group({
+      'addStaffData': formBuilder.group({
+        'emailAddress': ['',[
+          Validators.required,
+          Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+        ]],
+        'firstName': ['',Validators.required],
+        'lastName': ['', Validators.required],
+        'loginPassword': ['', Validators.required],
+        'role':['', Validators.required]
+
+      })
+    });
+  }
+  onSubmit(){
+    console.log("form for add staff on submit");
+    console.log(this.myAddStaffForm.value.addStaffData);
+    this.addStaffService.sendAddStaffDetails(this.myAddStaffForm.value.addStaffData).subscribe(
+      res=>{
+        if(res.success!=null){
+          console.log(res.success);
+        }
+        else if (res.data){
+          console.log(res.data);
+        }
+        else {
+          console.log(res.error);
+        }
+      }
+    )
+  }
+}
