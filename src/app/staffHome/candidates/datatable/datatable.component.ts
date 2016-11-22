0edb435f58a099b8ba3b58189f999b3261424550
bindu bhupathi <bindubhupathi12@gmail.com>
2016-11-22 14:20:58 -0500

    removed materialize css..

--- src/app/staffHome/candidates/datatable/datatable.component.ts
+++ src/app/staffHome/candidates/datatable/datatable.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {DatatableService} from "./datatable.service";
 import {Employee} from "./employee.interface";
 @Component({
   selector: 'app-datatable',
   templateUrl: './datatable.component.html',
-  styleUrls: ['./datatable.component.css']
+  styleUrls: ['./datatable.component.css'],
+  encapsulation:ViewEncapsulation.None,
 })
 export class DatatableComponent implements OnInit {
   employees:Employee[];
