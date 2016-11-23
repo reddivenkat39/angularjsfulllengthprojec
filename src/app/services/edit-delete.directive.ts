92c360f2aa88436dc6b43a8604176dfa335e4c79
chandu.adabala <chandu.adabala@tabnergc.com>
2016-11-23 10:26:45 -0500

    Merge branch 'feature/sprintNov' of http://10.10.5.33:7990/scm/por/tabnerportalui into feature/sprintNov
    # Conflicts:
    #	src/app/staffHome/candidates/candidates.component.html
    #	src/app/staffHome/candidates/datatable/datatable.component.html

---
+++ src/app/services/edit-delete.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appEditDelete]'
+})
+export class EditDeleteDirective {
+
+  constructor() { }
+
+}
---
+++ src/app/staffHome/candidates/all-candidates/details-by-id/details-by-id.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-by-id',
+  templateUrl: './details-by-id.component.html',
+  styleUrls: ['./details-by-id.component.css']
+})
+export class DetailsByIdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
