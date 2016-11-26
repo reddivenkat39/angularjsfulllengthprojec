611b2438723db74805113fa8a1e6ca64b0791382
chandu.adabala <chandu.adabala@tabnergc.com>
2016-11-26 17:13:22 -0500

    Merge branch 'feature/sprintNov' of http://10.10.5.33:7990/scm/por/tabnerportalui into feature/sprintNov
    # Conflicts:
    #	src/app/staffHome/employees/employees.component.css
    #	src/app/staffHome/employees/employees.component.ts

--- src/app/staffHome/employees/Allemployeesdetails/employees.component.ts
+++ src/app/staffHome/employees/Allemployeesdetails/employees.component.ts
@@ -1,43 +1,86 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService} from "../employee.service";
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {EmployeeService} from "../employee.service";
+import {Employee} from "../Employee";
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: 'employees.component.html',
-  styleUrls: ['employees.component.css']
+  selector     : 'app-employees',
+  templateUrl  : 'employees.component.html',
+  styleUrls    : ['employees.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class EmployeesComponent implements OnInit {
   private allEmployee = false;
-  private allActive =false;
+  private allActive = false;
   private allInActive = false;
-  constructor(private employeeService: EmployeeService) { }
+  allEmployees: Employee[];
+  allActiveEmployees: Employee[];
+  isTermDate = '';
+
+  constructor(private employeeService: EmployeeService) {
+  }
 
   ngOnInit() {
-    this.employeeService.getAllEmployeeDetails().subscribe(
-    res=>{
-      if(res.datares!=null){
-       console.log("yes getting data ", res.datares);
 
-      }else if(res.successres!=null){
-        console.log("success ", res.successres);
-      }else if(res.errorres!=null){
-        console.log("OOPs no data  found", res.errorres);
-      }else{
-        console.log("server problem ");
+  }
+
+
+  onAllEmployeeClicked() {
+    this.allEmployee = !this.allEmployee;
+    this.allActive = false;
+    this.allInActive = false;
+    this.employeeService.getAllEmployeeDetails().subscribe(
+      res => {
+        if (res.datares != null) {
+          console.log("yes getting data ", res.datares);
+          this.allEmployees = res.datares;
+          console.log("term date ...", res.datares);
+
+        } else if (res.successres != null) {
+          console.log("success ", res.successres);
+        } else if (res.errorres != null) {
+          console.log("OOPs no data  found", res.errorres);
+        } else {
+          console.log("server problem ");
+        }
       }
-    }
     );
   }
 
+  onAllActiveClicked() {
+    this.allActive = !this.allActive;
+    this.allEmployee = false;
+    this.allInActive = false;
+    this.employeeService.getAllEmployeeDetails().subscribe(
+      res => {
+        if (res.datares != null) {
+          console.log("yes getting data ", res.datares);
+          // this.allActiveEmployees = res.datares;
 
-  onAllEmployeeClicked(){
-    this.allEmployee=!this.allEmployee;
+          console.log("term date", this.allActiveEmployees);
+          if (res.datares.termDate === null || res.datares.termDate.equals('')) {
+            console.log("inactive employees got")
+          }
+          else {
+            this.allEmployees = res.datares;
+
+          }
+
+        } else if (res.successres != null) {
+          console.log("success ", res.successres);
+        } else if (res.errorres != null) {
+          console.log("OOPs no data  found", res.errorres);
+        } else {
+          console.log("server problem ");
+        }
+      }
+    );
+
   }
-  onAllActiveClicked(){
-    this.allActive=!this.allActive;
-  }
-  onAlLInActiveClicked(){
-    this.allInActive=!this.allInActive;
+
+  onAllInActiveClicked() {
+    this.allInActive = !this.allInActive;
+    this.allActive = false;
+    this.allEmployee = false;
   }
 
 }
--- src/app/staffHome/employees/employee.service.ts
+++ src/app/staffHome/employees/employee.service.ts
@@ -8,7 +8,7 @@ export class EmployeeService {
 
   getAllEmployeeDetails(){
     const headersSend = new Headers({'Content-Type':'application/json'});
-    return this.http.post("http://localhost:8080/getemployeepersonalinfo",{headers: headersSend}).map(
+    return this.http.get("http://localhost:8080/getemployeepersonalinfo",{headers: headersSend}).map(
       (res:Response)=> {
         console.log("getting all employees ",res.json());
         return res.json();
