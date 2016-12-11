545b31585d1c70d74e6d9fac7016478407093c10
bindu bhupathi <bindubhupathi12@gmail.com>
2016-12-11 18:51:41 -0500

    All css changes

--- src/app/staffHome/employees/employee.service.ts
+++ src/app/staffHome/employees/employee.service.ts
@@ -18,11 +18,14 @@ export class EmployeeService {
   }
   getDetailedViewEachEmployee(empId){
     console.log("get detailed view of each employee invoked");
-    const headersSend = new Headers({'Content-Type':'application/json'});
-    return this.http.get("http://10.10.5.55:8080/getemployeepersonalinfo",{headers: headersSend}).map(
-      (res:Response)=> {
-        console.log("getting each employee details",res.json());
-        return res.json();
+        const headersSend = new Headers({'Content-Type':'application/json'});
+        const candBody = {empId : empId };
+        console.log("empId in bodySend",candBody);
+        console.log("empId in headersSend",headersSend);
+        return this.http.post("http://10.10.5.55:8080/getemployeepersonalbyid",candBody,{headers: headersSend}).map(
+          (res:Response)=> {
+            console.log("res.json from getemployeepersonalinfo By id",res.json());
+            return res.json();
       }
     );
   }
