1c085ac1b692fbbe4c0eb646ac2a39415f26d9f7
chandu.adabala <chandu.adabala@tabnergc.com>
2016-12-15 08:30:49 -0500

    Merge branch 'feature/sprintNov' of http://10.10.5.33:7990/scm/por/tabnerportalui into feature/sprintNov
    # Conflicts:
    #	src/app/staffHome/candidates/candidates.service.ts
    #	src/app/staffHome/employees/employee.service.ts
    #	src/app/staffHome/invoices/invoice.service.ts
    #	src/app/staffHome/vendors/vendors.service.ts
    #	src/app/users/file-upload/file-upload.component.ts
    #	src/app/users/userservices/addstaff.service.ts
    #	src/app/users/userservices/staff.service.ts

--- src/app/staffHome/invoices/invoice.service.ts
+++ src/app/staffHome/invoices/invoice.service.ts
@@ -11,7 +11,7 @@ export class InvoiceService {
   getAllInvoices(){
     const headersSend = new Headers({'Content-Type':'application/json'});
 
-    return this.http.get("http://10.10.5.55:8080/allinvoices",{headers: headersSend}).map(
+    return this.http.get("http://localhost:8080//allinvoices",{headers: headersSend}).map(
       (res:Response)=> {
         console.log("getting all invoices ",res.json());
         return res.json();
