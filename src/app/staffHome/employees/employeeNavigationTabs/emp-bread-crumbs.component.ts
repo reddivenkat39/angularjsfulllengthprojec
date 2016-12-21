207d7d5d9dc335b3a2f71335f658a61d007dac35
bindu bhupathi <bindubhupathi12@gmail.com>
2016-12-21 10:41:34 -0500

    employee on row select

---
+++ src/app/staffHome/employees/employeeNavigationTabs/emp-bread-crumbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-bread-crumbs',
+  templateUrl: './emp-bread-crumbs.component.html',
+  styleUrls: ['./emp-bread-crumbs.component.css']
+})
+export class EmpBreadCrumbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
